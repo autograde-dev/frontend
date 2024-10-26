225d2d328cb4c4d7d7ac4d5624a518e27a8ed47e
Andres2087 <andres2078@gmail.com>
2024-10-26 09:42:28 -0500

    Se agreron las variables de entorno

--- src/App.jsx
+++ src/App.jsx
@@ -11,16 +11,16 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate replace to="/login"/>} />
+        <Route path="/" element={<Navigate replace to="/login" />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/student" element={<StudentPage />} />
         <Route path="/teacher" element={<TeacherPage />} />
-        <Route path='/admin' element={<AdminPage/>}/>
-        <Route path="/formStudent" element={<FormStudentsPage/>}/>
-        <Route path="/formteacher" element={<FormTeacherPage/>} />
+        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/formStudent" element={<FormStudentsPage />} />
+        <Route path="/formteacher" element={<FormTeacherPage />} />
       </Routes>
     </Router>
   );
 }
 
-export default App;
+export default App;
